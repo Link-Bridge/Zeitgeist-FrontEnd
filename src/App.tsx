7c6aa9f7a310ef72081c7514022662ac2855f469
Daniel Hurtado <a01707774@tec.mx>
2024-03-31 10:40:33 -0600

    feat(router): add react router

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,58 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Link
+} from 'react-router-dom';
+import React from 'react';
+import Projects from './pages/Projects';
+import Tasks from './pages/Tasks';
+import Clients from './pages/Clients';
+import Employees from './pages/Employees';
 import './App.css'
+import Home from './pages/Home';
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
+        <Router>
+      <div>
+        <nav>
+          {/* Navegación */}
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/projects">Projects</Link>
+            </li>
+            <li>
+              <Link to="/tasks">Tasks</Link>
+            </li>
+            <li>
+              <Link to="/clients">Clients</Link>
+            </li>
+            <li>
+              <Link to="/employees">Employees</Link>
+            </li>
+          </ul>
+        </nav>
+
+        {/* Configuración de las rutas */}
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/projects" element={<Projects />} />
+          <Route path="/tasks" element={<Tasks />} />
+          <Route path="/clients" element={<Clients />} />
+          <Route path="/employees" element={<Employees />} />
+        </Routes>
+      </div>
+    </Router>
       <div className='flex'>
         <a href="https://vitejs.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
---
+++ src/pages/Tasks/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Tasks = () => {
+  return (
+    <div>
+      <h1>Tasks Page</h1>
+      <p>Welcome to the Tasks page!</p>
+    </div>
+  );
+}
+
+export default Tasks;
