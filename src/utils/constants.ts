3e035e3ccf0e4931b82ae9e4e9fc89c4f7e292da
Daniel Hurtado <a01707774@tec.mx>
2024-04-04 22:49:30 -0600

    feat(init): add base files

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,3 +1,11 @@
 export enum EnvKeysValues {
     BASE_API_URL = 'https://api.example.com',
 }
+
+export enum Routes {
+    HOME = '/',
+    PROJECTS = '/projects',
+    TASKS = '/tasks',
+    CLIENTS = '/clients',
+    EMPLOYEES = '/employees',
+}
