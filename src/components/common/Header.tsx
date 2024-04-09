ffbd7d045e397984fa266eafd04be82562947a9b
DenisseDB <A01702603@tec.mx>
2024-04-09 15:59:34 -0600

    feat(component): Header

--- src/components/common/Header.tsx
+++ src/components/common/Header.tsx
@@ -1,33 +1,34 @@
 import { useEffect, useState } from 'react';
-import notifs from '../../assets/icons/notifications.svg'
-import profile from '../../assets/icons/profile.png'
+import notification from '../../assets/icons/notifications.svg'
+import { Avatar } from '@mui/joy';
+import Colors from '../../colors'
 
 interface HeaderProps {
-    pageTitle: string;
+  pageTitle: string;
 }
 
 const Header = ({pageTitle}:HeaderProps) => {
-    const [currentDate, setCurrentDate] = useState('');
-    useEffect(() => {
-        const date = new Date();
-        const dateOption = {weekday: 'long', day: 'numeric', month: 'long', year: 'numeric'};
-        setCurrentDate(date.toLocaleDateString('en-US', dateOption));
-    }, [])
+  const [currentDate, setCurrentDate] = useState('');
+  useEffect(() => {
+    const date = new Date();
+    const dateOption = {weekday: 'long', day: 'numeric', month: 'long', year: 'numeric'};
+    setCurrentDate(date.toLocaleDateString('en-US', dateOption));
+  }, [])
     
-    return (
-        <header className='flex flex-row justify-between items-center pl-[300px]'>
-          <div >
-            <h1 className='text-[#C29A51]'>{pageTitle}</h1>
-            <p>{currentDate}</p>
-          </div>
-          <div className='flex flex-row justify-between items-center g-7 p-7'>
-            <button className='bg-[#D9D9D9]'><img className= 'h-8 w-8' src={notifs} alt="Notifications" /></button>
-            <img className='h-12 w-12' src={profile} alt="userPhoto"/>
-          </div>
-        </header>
-        
-      );
-    };
+  return (
+    <header className='flex flex-row flex-wrap justify-between items-center pl-[280px] pr-10 p-5'>
+      <section>
+        <h1 style={{color: Colors.gold,fontFamily:'Didot', fontSize: '2.8rem', lineHeight: '1.1'}}>{pageTitle}</h1>
+        <p className='p-2'>{currentDate}</p>
+      </section>
+      
+      <section className='flex flex-row align-items justify-between items-center g-10'>
+        <button className='p-4'><img src={notification} alt="Notifications" /></button>
+        <Avatar variant='solid'>OP</Avatar>
+      </section>
+    </header>
+  );
+};
 
 export default Header;
 
