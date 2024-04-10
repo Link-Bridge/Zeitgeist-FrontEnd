79e8d057105903ef2e5eaf61b9e4d9530a118f39
Olimpia Garcia <olimpia.gh24@gmail.com>
2024-04-09 19:15:11 -0600

    feat(component): add chip button

--- src/components/common/DropDown.tsx
+++ src/components/common/DropDown.tsx
@@ -1,28 +1,22 @@
 import { ReactNode } from 'react';
 import Chip from '@mui/joy/Chip';
 import Box from '@mui/joy/Box';
-import CheckIcon from '@mui/icons-material/Check';
+import KeyboardArrowDownIcon from '@mui/icons-material/KeyboardArrowDown';
 
 import Menu from '@mui/joy/Menu';
 import MenuButton from '@mui/joy/MenuButton';
 import MenuItem from '@mui/joy/MenuItem';
 import Dropdown from '@mui/joy/Dropdown';
+import { Option, Select } from '@mui/joy';
 
 
 export default function ClickableChip() {
     return (
-      <Box sx={{ display: 'flex', gap: 1, alignItems: 'center' }}>
-        <Chip
-          variant="outlined"
-          color="neutral"
-          size="lg"
-          startDecorator={<Avatar size="sm" src={`/static/images/avatar/1.jpg`} />}
-          endDecorator={<CheckIcon fontSize="md" />}
-          onClick={() => alert('You clicked the Joy Chip!')}
-        >
-          Mark
+    
+        <Chip component={Select} indicator>
+          <Option value={"Hola"}> Hola</Option>
         </Chip>
-      </Box>
+
     );
   }
 
--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,11 +1,15 @@
 import Layout from "../../components/common/Layout";
+import DropDown from "../../components/common/DropDown";
 
 const Home = () => {
   return (
     <Layout>
       <main className="p-10 py-4 flex gap-4">
-        <section className="bg-[#FAFAFA] rounded-xl basis-4/6 h-[750px] p-10"><h2 className="font-semibold text-[34px]">My Projects</h2></section>
+        <section className="bg-[#FAFAFA] rounded-xl basis-4/6 h-[750px] p-10"><h2 className="font-semibold text-[34px]">My Projects</h2>
+        <DropDown></DropDown>
+        </section>
         <section className="bg-[#FAFAFA] rounded-xl basis-2/6 h-[750px] p-10"><h2 className="font-semibold text-[34px]">Clients</h2></section>
+        
       </main>
     </Layout>
   );
