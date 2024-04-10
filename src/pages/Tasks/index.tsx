b891cc852e07145f8868d1f7591a60dfc6435779
Ricardo Fernandez <ricardo.a.fernandez.a@gmail.com>
2024-04-10 15:53:55 -0600

    fix(UI): Layout now does not re-render when changing pages

--- src/pages/Tasks/index.tsx
+++ src/pages/Tasks/index.tsx
@@ -1,13 +1,9 @@
-import Layout from "../../components/common/Layout";
-
 const Tasks = () => {
   return (
-    <Layout>
-      <main className="p-10 py-4 flex gap-4">
-        <h1>Tasks Page</h1>
-        <p>Welcome to the Tasks page!</p>
-      </main>
-    </Layout>
+    <main className='p-10 py-4 flex gap-4'>
+      <h1>Tasks Page</h1>
+      <p>Welcome to the Tasks page!</p>
+    </main>
   );
 };
 
