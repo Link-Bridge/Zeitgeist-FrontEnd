762fc2a8bfd0ff94e93030d4522b352c4850ab17
Ricardo Fernandez <ricardo.a.fernandez.a@gmail.com>
2024-04-09 22:42:37 -0600

    fix(UI): Changed svgs for icons and minor UI fixes

--- src/components/common/Header.tsx
+++ src/components/common/Header.tsx
@@ -1,13 +1,13 @@
-import { useEffect, useState } from 'react';
-import notification from '../../assets/icons/notifications.svg'
+import { NotificationsNone } from '@mui/icons-material';
 import { Avatar } from '@mui/joy';
-import Colors from '../../colors'
+import { useEffect, useState } from 'react';
+import Colors from '../../colors';
 
 interface HeaderProps {
   pageTitle: string;
 }
 
-const Header = ({pageTitle}:HeaderProps) => {
+const Header = ({ pageTitle }: HeaderProps) => {
   const [currentDate, setCurrentDate] = useState('');
   useEffect(() => {
     const date = new Date();
@@ -15,20 +15,32 @@ const Header = ({pageTitle}:HeaderProps) => {
       weekday: 'long',
       day: 'numeric',
       month: 'long',
-      year: 'numeric'
+      year: 'numeric',
     };
     setCurrentDate(date.toLocaleDateString('en-US', dateOptions));
-  }, [])
-    
+  }, []);
+
   return (
-    <header className='flex flex-row flex-wrap justify-between items-center pl-[280px] pr-10 p-5'>
+    <header className='flex flex-row flex-wrap justify-between items-start pl-[280px] pr-10 p-5'>
       <section>
-        <h1 style={{color: Colors.gold,fontFamily:'Didot', fontSize: '2.8rem', lineHeight: '1.1'}}>{pageTitle}</h1>
-        <p className='p-2'>{currentDate}</p>
+        <h1
+          style={{
+            color: Colors.gold,
+            fontFamily: 'Didot',
+            fontSize: '3.5rem',
+            lineHeight: '1.1',
+            letterSpacing: '1.5px',
+          }}
+        >
+          {pageTitle}
+        </h1>
+        <p className='py-2 text-[#686868]'>{currentDate}</p>
       </section>
-      
-      <section className='flex flex-row align-items justify-between items-center g-10'>
-        <button className='p-4'><img src={notification} alt="Notifications" /></button>
+
+      <section className='flex flex-row align-items justify-between items-center g-10 mt-3'>
+        <button className='mx-8 text-[#C29A51]'>
+          <NotificationsNone fontSize='large' />
+        </button>
         <Avatar variant='solid'>OP</Avatar>
       </section>
     </header>
@@ -36,4 +48,3 @@ const Header = ({pageTitle}:HeaderProps) => {
 };
 
 export default Header;
-
