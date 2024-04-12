f964ce3e5540ac0cb800f3ce08dc70d0dc163b79
Ricardo Fernandez <ricardo.a.fernandez.a@gmail.com>
2024-04-12 17:49:32 -0600

    fix(UI): change icons to mui/icons library

--- src/components/common/SideBar.tsx
+++ src/components/common/SideBar.tsx
@@ -1,25 +1,21 @@
+import { FolderShared, Home, SwitchAccount, Toc, ViewTimeline } from '@mui/icons-material';
 import { Link } from 'react-router-dom';
 import LogoZeitgeist from '../../assets/icons/LOGO_Zeitgeist.svg';
-import ClientsIcon from '../../assets/icons/folder_shared.svg';
-import EmployeesIcon from '../../assets/icons/groups_black.svg';
-import HomeIcon from '../../assets/icons/homeIcon.svg';
-import TasksIcon from '../../assets/icons/list.svg';
-import ProjectsIcon from '../../assets/icons/view_module.svg';
 import Colors from '../../colors';
 import { RoutesPath } from '../../utils/constants';
 
 const Items = [
-  { icon: HomeIcon, href: RoutesPath.HOME, title: 'Home Page' },
-  { icon: ProjectsIcon, href: RoutesPath.PROJECTS, title: 'Projects' },
-  { icon: TasksIcon, href: RoutesPath.TASKS, title: 'Tasks' },
-  { icon: ClientsIcon, href: RoutesPath.CLIENTS, title: 'Clients' },
-  { icon: EmployeesIcon, href: RoutesPath.EMPLOYEES, title: 'Employees' },
+  { icon: Home, href: RoutesPath.HOME, title: 'Home Page' },
+  { icon: ViewTimeline, href: RoutesPath.PROJECTS, title: 'Projects' },
+  { icon: Toc, href: RoutesPath.TASKS, title: 'Tasks' },
+  { icon: FolderShared, href: RoutesPath.CLIENTS, title: 'Clients' },
+  { icon: SwitchAccount, href: RoutesPath.EMPLOYEES, title: 'Employees' },
 ];
 
 const SideBar = () => {
   return (
     <aside className='bg-[#424242] h-screen flex flex-col items-center pt-16 gap-10 w-[200px]'>
-      <Link to={'/'}>
+      <Link to={RoutesPath.HOME}>
         <img src={LogoZeitgeist} alt='Zeitgeist Logo' className='w-16 mb-10' />
       </Link>
       <nav className='w-full flex justify-center'>
