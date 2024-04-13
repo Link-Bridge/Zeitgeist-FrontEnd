ec2384e84e5ecdc5145c64ac182351917007af48
Ricardo Fernandez <ricardo.a.fernandez.a@gmail.com>
2024-04-13 02:23:02 -0600

    feat(newProject): custom hook now handles post logic

--- src/hooks/useNewProject.ts
+++ src/hooks/useNewProject.ts
@@ -1,4 +1,5 @@
-import { useReducer } from 'react';
+import axios from 'axios';
+import { FormEvent, useReducer, useState } from 'react';
 
 export interface FormState {
   projectName: string;
@@ -44,12 +45,26 @@ const formReducer = (state: FormState, action: FormAction) => {
 
 const useNewProject = () => {
   const [formState, dispatch] = useReducer(formReducer, initialFormState);
+  const [error, setError] = useState<Error | null>(null);
 
   const handleChange = (field: keyof FormState, value: string | Date | boolean) => {
     dispatch({ type: 'CHANGE', field, value });
   };
 
-  return { formState, handleChange };
+  const handleSubmit = async (e: FormEvent) => {
+    e.preventDefault();
+    try {
+      await axios.post('http://localhost:4000/api/v1/project', formState);
+    } catch (err: unknown) {
+      if (axios.isAxiosError(err)) {
+        setError(new Error(err.response?.data.message || err.message));
+      } else {
+        setError(new Error('Unknown error ocurred'));
+      }
+    }
+  };
+
+  return { formState, handleChange, handleSubmit, error };
 };
 
 export default useNewProject;
