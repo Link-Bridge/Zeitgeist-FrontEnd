3a3143ef4052415f94d4c485edc731a6a8079f50
yna2131 <jn7924@gmail.com>
2024-04-13 16:53:33 -0600

    feat(deleteModal): Add simple content to the modal

--- src/components/common/DeleteModal.tsx
+++ src/components/common/DeleteModal.tsx
@@ -1,12 +1,26 @@
 import { Delete } from '@mui/icons-material';
-import { IconButton } from '@mui/joy';
-// import * as React from 'react';
+import DialogContent from '@mui/joy/DialogContent';
+import DialogTitle from '@mui/joy/DialogTitle';
+import IconButton from '@mui/joy/IconButton';
+import Modal from '@mui/joy/Modal';
+import ModalClose from '@mui/joy/ModalClose';
+import ModalDialog from '@mui/joy/ModalDialog';
+import * as React from 'react';
 
 export default function DeleteModal() {
-  // const [open, setOpen] = React.useState<boolean>(false);
+  const [open, setOpen] = React.useState<boolean>(false);
   return (
-    <IconButton>
-      <Delete />
-    </IconButton>
+    <React.Fragment>
+      <IconButton onClick={() => setOpen(true)}>
+        <Delete />
+      </IconButton>
+      <Modal open={open} onClose={() => setOpen(false)}>
+        <ModalDialog>
+          <ModalClose />
+          <DialogTitle>Delete Employee</DialogTitle>
+          <DialogContent>Are you sure you want to delete this employee?</DialogContent>
+        </ModalDialog>
+      </Modal>
+    </React.Fragment>
   );
 }
