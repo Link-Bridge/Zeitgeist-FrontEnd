ebcdeaea4332d1c2cdadb59259f963bbf8b2b5e3
sgarnica1 <sgarnica1902@gmail.com>
2024-04-12 18:28:30 -0600

    chore(loader): Add loader

--- src/pages/Clients/index.tsx
+++ src/pages/Clients/index.tsx
@@ -4,31 +4,36 @@ import ClientCard from "../../components/common/ClientCard";
 import { CompanyEntity } from "../../types/company";
 import useFetch from "../../hooks/useFetch";
 import { Response } from "../../types/response";
+import Loader from "../../components/common/Loader";
 
 const Clients = () => {
   const { data, isLoading, error } = useFetch<Response<CompanyEntity[]>>('http://localhost:4000/company')
   const companies: CompanyEntity[] = (data && data.data) ? data.data.flat() : [];
 
   return (
-    <main className="p-10 py-0 flex flex-col gap-4">
+    <main className="p-10 py-0 flex flex-col">
       <section className='flex flex-row justify-end p-5 gap-6'>
         <button>Not Archived</button>
         <AddButton />
       </section>
-      <CardsGrid>
-        {isLoading && <p>Loading...</p>}
+      <div className="flex justify-center w-full">
+        {isLoading && <Loader />}
         {error && <p>Error loading companies.</p>}
-        {!isLoading && !error && companies && companies.map((company) => (
-          <ClientCard
-            key={company.id}
-            name={company.name}
-            accountingHours={company.accountingHours || 0}
-            legalHours={company.legalHours || 0}
-            chargeableHours={company.chargeableHours || 0}
-            totalProjects={company.totalProjects || 0}
-          />
-        ))}
-      </CardsGrid>
+      </div>
+      {!isLoading && !error && companies && (
+        <CardsGrid>
+          {companies.map((company) => (
+            <ClientCard
+              key={company.id}
+              name={company.name}
+              accountingHours={company.accountingHours || 0}
+              legalHours={company.legalHours || 0}
+              chargeableHours={company.chargeableHours || 0}
+              totalProjects={company.totalProjects || 0}
+            />
+          ))}
+        </CardsGrid>
+      )}
     </main>
   );
 };
