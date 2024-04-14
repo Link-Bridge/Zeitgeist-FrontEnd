8fcaf45dbb35daf00e14c630480d16520592d637
yna2131 <jn7924@gmail.com>
2024-04-13 20:29:47 -0600

    chore(deleteModal): Clean up the test components

--- src/pages/Clients/index.tsx
+++ src/pages/Clients/index.tsx
@@ -1,25 +1,8 @@
-import { Delete } from '@mui/icons-material';
-import { useState } from 'react';
-import DeleteModal from '../../components/common/DeleteModal';
-
 const Clients = () => {
-  const [open, setOpen] = useState(false);
-
-  const openModal = () => {
-    setOpen(true);
-  };
-
   return (
     <main className='p-10 py-4 flex gap-4'>
       <h1>Clients Page</h1>
       <p>Welcome to the Clients page!</p>
-      <Delete onClick={openModal} />
-      <DeleteModal
-        open={open}
-        title='Delete Client'
-        description='Are you sure to delete this client?'
-        setOpen={setOpen}
-      />
     </main>
   );
 };
