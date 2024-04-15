1480a5214e23b8360e1540efc33513168c71a107
DenisseDB <A01702603@tec.mx>
2024-04-15 12:31:58 -0600

    feat(NewClientModal): create new client form modal

---
+++ src/components/NewClientFormModal.tsx
@@ -0,0 +1,79 @@
+import { TextField } from '@mui/material';
+import Box from '@mui/material/Box';
+import Button from '@mui/material/Button';
+import Modal from '@mui/material/Modal';
+import Typography from '@mui/material/Typography';
+
+const style = {
+  position: 'absolute' as 'absolute',
+  top: '50%',
+  left: '50%',
+  transform: 'translate(-50%, -50%)',
+  width: 620,
+  bgcolor: 'background.paper',
+  border: '2px solid #000',
+  boxShadow: 24,
+  p: 4,
+};
+
+interface NewClientFormModalProps {
+  open: boolean;
+  onClose: () => void;
+}
+
+const NewClientFormModal = ({ open, onClose }: NewClientFormModalProps) => {
+  return (
+    <Modal
+      open={open}
+      onClose={onClose}
+      aria-labelledby='modal-modal-title'
+      aria-describedby='modal-modal-description'
+      BackdropProps={{onClick: () => {}}}
+    >
+      <Box sx={style}>
+        <Typography id='modal-modal-title' variant='h6' component='h2'>
+          New Client Form
+        </Typography>
+
+        <Typography id='modal-modal-description' sx={{ mt: 2 }}>
+          This is where you can add a new client.
+        </Typography>
+
+        <Box
+          component='form'
+          sx={{
+            '& .MuiTextField-root': { m: 1, width: '30ch' },
+            display: 'flex',
+            flexDirection: 'column',
+            alignItems: 'center'
+          }}
+          noValidate
+          autoComplete='off'
+        >
+          <div className='centered-input'>
+            <TextField required id='clientName' label='Clients name' variant='outlined' />
+          </div>
+
+          <Box sx={{ display: 'flex', flexdirection: 'row' }}>
+            <TextField required id='clientEmail' label='Email' variant='outlined' />
+            <TextField required id='clientPhone' label='Phone number' variant='outlined' />
+          </Box>
+
+          <Box sx={{ display: 'flex', flexdirection: 'row' }}>
+            <TextField required id='clientRFC' label='RFC' variant='outlined' />
+            <TextField required id='clientConstituton' label='Constitucion' variant='outlined' />
+          </Box>
+
+          <Box sx={{ display: 'flex', flexdirection: 'row' }}>
+            <TextField required id='clientSocialDomain' label='Social Domain' variant='outlined' />
+            <TextField required id='clientFiscalDomain' label='Fiscal Domain' variant='outlined' />
+          </Box>
+        </Box>
+          <Button type='submit'>Enviar</Button>
+        <Button onClick={onClose}>Close</Button>
+      </Box>
+    </Modal>
+  );
+};
+
+export default NewClientFormModal;
--- src/components/common/AddButton.tsx
+++ src/components/common/AddButton.tsx
@@ -1,9 +1,13 @@
 import Button from '@mui/joy/Button';
 import colors from '../../colors'; // Importar los colores del archivo colors.tsx
 
-function AddButton() {
+interface AddButtonProps {
+  onClick: () => void;
+}
+
+function AddButton({ onClick } : AddButtonProps) {
   return (
-    <Button
+    <Button onClick={onClick}
       variant='solid'
       size='sm'
       sx={{
