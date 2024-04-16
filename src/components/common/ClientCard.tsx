25b634703eac53171015a54e01242df26bc62432
DenisseDB <A01702603@tec.mx>
2024-04-16 17:03:44 -0600

    style(color): change company name color

--- src/components/common/ClientCard.tsx
+++ src/components/common/ClientCard.tsx
@@ -13,26 +13,26 @@ interface ClientCardProps {
 const ClientCard = ({name, accountingHours, legalHours, chargeableHours, totalProjects} : ClientCardProps) => {
   return (
     <CardContainer>
-        <header className='mb-3 text-xl flex'>
+        <header className='mb-3 text-xl flex text-[#876F39]' >
             <img src={WorkIcon} alt="Yellow Briefcase" className="mr-2" /> {name}
         </header>
         <div className='grid grid-cols-2 gap-2'>
             <Chip sx={{
                 bgcolor:'#EAD3AA',
-                fontSize: '13px',
+                fontSize: '12px',
             }}>Accounting Hours: {accountingHours}</Chip>
             <Chip sx={{
                 bgcolor: '#EAD3AA'
             }}>Legal Hours: {legalHours}</Chip>
             <Chip sx={{
                 bgcolor:'#B39C844C',
-                fontSize: '13px',
+                fontSize: '12px',
             }}>Chargeable Hours: {chargeableHours}</Chip>
             <Chip sx={{
                 bgcolor:'#E3EFFB',
                 color:'#0B6BCB',
                 border: '1px solid #97C3F0',
-                fontSize: '13px',
+                fontSize: '12px',
             }}>Total Projects: {totalProjects}</Chip>
         </div>
     </CardContainer>
