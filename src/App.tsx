36bc1a57865ac5b4fc2c3c0de118721dd4e15dcb
Carlos Salguero <carlossalguero441@gmail.com>
2024-04-18 17:00:44 -0600

    refactor(App): added localization provider again

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,5 @@
+import { LocalizationProvider } from '@mui/x-date-pickers';
+import { AdapterMoment } from '@mui/x-date-pickers/AdapterMoment';
 import { Outlet, Route, BrowserRouter as Router, Routes } from 'react-router-dom';
 import Layout from './components/common/Layout';
 import ProtectedRoute from './components/common/ProtectedRoute';
@@ -7,60 +9,59 @@ import Employees from './pages/Employees';
 import Home from './pages/Home';
 import Projects from './pages/Projects';
 import Tasks from './pages/Tasks';
-import NewTask from './pages/Tasks/new';
 import { RoutesPath } from './utils/constants';
-import { LocalizationProvider } from '@mui/x-date-pickers';
-import { AdapterMoment } from '@mui/x-date-pickers/AdapterMoment';
 
 function App() {
   return (
-    <Router>
-      <Routes>
-        <Route path={RoutesPath.ROOT} element={<Auth />} />
-        <Route element={<Layout children={<Outlet />} />}>
-          <Route
-            path={RoutesPath.HOME}
-            element={
-              <ProtectedRoute>
-                <Home />
-              </ProtectedRoute>
-            }
-          />
-          <Route
-            path={RoutesPath.PROJECTS}
-            element={
-              <ProtectedRoute>
-                <Projects />
-              </ProtectedRoute>
-            }
-          />
-          <Route
-            path={RoutesPath.TASKS}
-            element={
-              <ProtectedRoute>
-                <Tasks />
-              </ProtectedRoute>
-            }
-          />
-          <Route
-            path={RoutesPath.CLIENTS}
-            element={
-              <ProtectedRoute>
-                <Clients />
-              </ProtectedRoute>
-            }
-          />
-          <Route
-            path={RoutesPath.EMPLOYEES}
-            element={
-              <ProtectedRoute>
-                <Employees />
-              </ProtectedRoute>
-            }
-          />
-        </Route>
-      </Routes>
-    </Router>
+    <LocalizationProvider dateAdapter={AdapterMoment}>
+      <Router>
+        <Routes>
+          <Route path={RoutesPath.ROOT} element={<Auth />} />
+          <Route element={<Layout children={<Outlet />} />}>
+            <Route
+              path={RoutesPath.HOME}
+              element={
+                <ProtectedRoute>
+                  <Home />
+                </ProtectedRoute>
+              }
+            />
+            <Route
+              path={RoutesPath.PROJECTS}
+              element={
+                <ProtectedRoute>
+                  <Projects />
+                </ProtectedRoute>
+              }
+            />
+            <Route
+              path={RoutesPath.TASKS}
+              element={
+                <ProtectedRoute>
+                  <Tasks />
+                </ProtectedRoute>
+              }
+            />
+            <Route
+              path={RoutesPath.CLIENTS}
+              element={
+                <ProtectedRoute>
+                  <Clients />
+                </ProtectedRoute>
+              }
+            />
+            <Route
+              path={RoutesPath.EMPLOYEES}
+              element={
+                <ProtectedRoute>
+                  <Employees />
+                </ProtectedRoute>
+              }
+            />
+          </Route>
+        </Routes>
+      </Router>
+    </LocalizationProvider>
   );
 }
 
