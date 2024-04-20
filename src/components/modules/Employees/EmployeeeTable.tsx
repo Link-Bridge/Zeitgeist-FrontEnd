d58351650adb7257b92ecd55d7e0b3fa02d1cec8
Olimpia Garcia <olimpia.gh24@gmail.com>
2024-04-19 18:35:27 -0600

    feat(employeeTable): add google image to photo avatar

--- src/components/modules/Employees/EmployeeeTable.tsx
+++ src/components/modules/Employees/EmployeeeTable.tsx
@@ -4,14 +4,17 @@ import KeyboardArrowDown from '@mui/icons-material/KeyboardArrowDown';
 import { IconButton, Option, Select, selectClasses } from '@mui/joy';
 import Table from '@mui/joy/Table';
 import Chip from '@mui/joy/chip';
-import { useState } from 'react';
+import { Avatar } from '@mui/material';
+import { useContext, useEffect, useState } from 'react';
 import colors from '../../../colors';
+import { SnackbarContext } from '../../../hooks/snackbarContext';
 import useFetch from '../../../hooks/useFetch';
 import { Response } from '../../../types/response';
 import DeleteModal from '../../common/DeleteModal';
+import Loader from '../../common/Loader';
 
 type Employee = {
-  photo: string;
+  imageUrl?: string;
   firstName: string;
   lastName: string;
   email: string;
@@ -19,64 +22,83 @@ type Employee = {
 };
 
 export default function EmployeeTable() {
+  const { setState } = useContext(SnackbarContext);
   const [open, setOpen] = useState(false);
   const req = useFetch<Response<Employee>>('http://localhost:4000/api/v1/employee');
   const openModal = () => setOpen(true);
+  console.log(req.data?.data);
+
+  useEffect(() => {
+    if (req.error) {
+      setState({ open: true, message: req.error.message, type: 'danger' });
+    }
+  }, [req.error, setState]);
 
   return (
     <Table variant={'outlined'}>
-      <thead>
-        <tr>
-          <th style={{ width: '10%' }}> Photo </th>
-          <th>Name</th>
-          <th>Role</th>
-          <th>Email</th>
-          <th style={{ width: '10%' }}></th>
-        </tr>
-      </thead>
-      <tbody>
-        {!req.isLoading &&
-          req.data?.data.map(employee => (
+      {req.isLoading ? (
+        <Loader />
+      ) : (
+        <>
+          <thead>
             <tr>
-              <td>
-                {' '}
-                <AccountCircleIcon></AccountCircleIcon>{' '}
-              </td>
-              <td>{employee.firstName} {employee.lastName} </td>
-              <td>
-                <Select
-                  variant='outlined'
-                  color='neutral'
-                  indicator={<KeyboardArrowDown />}
-                  sx={{
-                    [`& .${selectClasses.indicator}`]: {
-                      transition: '0.2s',
-                      [`&.${selectClasses.expanded}`]: {
-                        transform: 'rotate(-180deg)',
-                      },
-                    },
-                  }}
-                >
-                  <Option value='employee.role'>{employee.role}</Option>
-                </Select>
-              </td>
-              <td>
-                <Chip variant='soft'> {employee.email} </Chip>
-              </td>
-              <td>
-                <IconButton>
-                  <DeleteOutlineIcon onClick={openModal} style={{ color: colors.gold }} />
-                </IconButton>
-              </td>
+              <th style={{ width: '10%' }}> Photo </th>
+              <th>Name</th>
+              <th>Role</th>
+              <th>Email</th>
+              <th style={{ width: '10%' }}></th>
             </tr>
-          ))}
-      </tbody>
-      <DeleteModal
-        open={open}
-        title='Delete Employee'
-        description='Are you sure you want to delete this employee?'
-        setOpen={setOpen}
-      />
+          </thead>
+          <tbody>
+            {!req.isLoading &&
+              req.data?.data.map(employee => (
+                <tr>
+                  <td>
+                    {employee.imageUrl ? (
+                      <Avatar src={employee.imageUrl}></Avatar>
+                    ) : (
+                      <AccountCircleIcon></AccountCircleIcon>
+                    )}
+                  </td>
+                  <td>
+                    {employee.firstName} {employee.lastName}{' '}
+                  </td>
+                  <td>
+                    <Select
+                      variant='outlined'
+                      color='neutral'
+                      indicator={<KeyboardArrowDown />}
+                      sx={{
+                        [`& .${selectClasses.indicator}`]: {
+                          transition: '0.2s',
+                          [`&.${selectClasses.expanded}`]: {
+                            transform: 'rotate(-180deg)',
+                          },
+                        },
+                      }}
+                    >
+                      <Option value='employee.role'>{employee.role}</Option>
+                    </Select>
+                  </td>
+                  <td>
+                    <Chip variant='soft'> {employee.email} </Chip>
+                  </td>
+                  <td>
+                    <IconButton>
+                      <DeleteOutlineIcon onClick={openModal} style={{ color: colors.gold }} />
+                    </IconButton>
+                  </td>
+                </tr>
+              ))}
+          </tbody>
+          <DeleteModal
+            open={open}
+            title='Delete Employee'
+            description='Are you sure you want to delete this employee?'
+            setOpen={setOpen}
+          />
+        </>
+      )}
     </Table>
   );
 }
