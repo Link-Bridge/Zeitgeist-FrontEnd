66244352496c24f346539602695fb1d332460b56
Ian326 <a01708940@tec.mx>
2024-04-27 02:35:44 -0600

    feat(RF09): Add Notification Tray

--- src/components/common/Header.tsx
+++ src/components/common/Header.tsx
@@ -1,16 +1,18 @@
-import { NotificationsNone, Palette } from '@mui/icons-material';
+import { NotificationsNone } from '@mui/icons-material';
 import { Avatar } from '@mui/joy';
 import { useEffect, useState } from 'react';
 import Colors from '../../colors';
 import { IconButton } from '@mui/material';
 import { styled } from '@mui/material/styles';
 import Badge, { BadgeProps } from '@mui/material/Badge';
+import Menu from '@mui/material/Menu';
+import MenuItem from '@mui/material/MenuItem';
 
 interface HeaderProps {
   pageTitle: string;
 }
 
-const StyledBadge = styled(Badge)<BadgeProps>(({ theme }) => ({
+const StyledBadge = styled(Badge)<BadgeProps>(() => ({
   '& .MuiBadge-badge': {
     right: -2,
     top: 0,
@@ -18,6 +20,15 @@ const StyledBadge = styled(Badge)<BadgeProps>(({ theme }) => ({
     padding: '0 4px',
   },
 }));
+
+const TempOptions = [
+  'Notification01',
+  'Notification02',
+  'Notification03',
+  'Notification04',
+  'Notification05',
+  'Notification06',
+];
 
 const Header = ({ pageTitle }: HeaderProps) => {
   const [currentDate, setCurrentDate] = useState('');
@@ -31,6 +42,15 @@ const Header = ({ pageTitle }: HeaderProps) => {
     };
     setCurrentDate(date.toLocaleDateString('en-US', dateOptions));
   }, []);
+
+  const [anchorEl, setAnchorEl] = useState<null | HTMLElement>(null);
+  const open = Boolean(anchorEl);
+  const handleClick = (event: React.MouseEvent<HTMLElement>) => {
+    setAnchorEl(event.currentTarget);
+  };
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
 
   return (
     <header className='flex flex-row flex-wrap justify-between items-start pt-2 basis-1/6'>
@@ -49,11 +69,40 @@ const Header = ({ pageTitle }: HeaderProps) => {
       </section>
 
       <section className='flex flex-row align-items justify-between items-center g-10 mt-3 space-x-2'>
-        <IconButton className='mx-8 text-[#C29A51]'>
+        <IconButton className='mx-8 text-[#C29A51]' onClick={handleClick}>
           <StyledBadge badgeContent={326} style={{ color: 'white' }}>
             <NotificationsNone fontSize='large' style={{color: Colors.gold}}/>
           </StyledBadge>
         </IconButton>
+        <Menu
+          id="long-menu"
+          MenuListProps={{
+            'aria-labelledby': 'long-button',
+          }}
+          anchorEl={anchorEl}
+          open={open}
+          onClose={handleClose}
+          PaperProps={{
+            style: {
+              maxHeight: '15ch',
+              width: '40ch',
+            },
+          }}
+          anchorOrigin={{
+            vertical: 'bottom',
+            horizontal: 'right',
+          }}
+          transformOrigin={{
+            vertical: 'top',
+            horizontal: 'right',
+          }}
+        >
+          {TempOptions.map((option) => (
+            <MenuItem key={option} onClick={handleClose}>
+              {option}
+            </MenuItem>
+          ))}
+        </Menu>
         <Avatar variant='solid'>OP</Avatar>
       </section>
     </header>
