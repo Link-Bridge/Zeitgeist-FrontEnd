bf49e6e525aa0a49117f4a7ac341adb7707c6e24
Frida Bailleres <a01708633@tec.mx>
2024-05-02 00:31:57 -0600

    fix(SideBar): alinear

--- src/components/common/SideBar.tsx
+++ src/components/common/SideBar.tsx
@@ -14,12 +14,14 @@ const Items = [
 
 const SideBar = () => {
   return (
-    <aside className="relative bg-[url('/src/assets/marmol.jpg')] bg-cover h-screen flex flex-col items-center w-[200px]">
+    <aside className="relative bg-[url('/src/assets/marmol.jpg')] bg-cover h-screen top-0 left-0 flex flex-col items-center pt-16 gap-10 w-[200px]">
       <div className='absolute top-0 left-0 w-full h-full bg-black bg-opacity-50'></div>
-      <div className='relative z-10 flex flex-col items-center w-full'>
-        <Link to={RoutesPath.HOME} className='w-full flex justify-center mt-16'>
-          <img src={LogoZeitgeist} alt='Zeitgeist Logo' className='w-16 mb-10' />
-        </Link>
+      <div className='relative z-10 w-full'>
+        <div className='flex justify-center'>
+          <Link to={RoutesPath.HOME}>
+            <img src={LogoZeitgeist} alt='Zeitgeist Logo' className='w-16 mb-10' />
+          </Link>
+        </div>
         <nav className='w-full'>
           <ul className='w-full'>
             {Items.map(item => (
@@ -29,11 +31,11 @@ const SideBar = () => {
               >
                 <Link
                   to={item.href}
-                  className='flex items-center justify-center gap-3 px-9 py-5'
+                  className='flex items-center gap-3 px-9 py-5'
                   style={{ color: colors.lightGold }}
                 >
-                  <item.icon style={{ color: colors.lightGold }} />
-                  <span>{item.title}</span>
+                  <item.icon></item.icon>
+                  <p>{item.title}</p>
                 </Link>
               </li>
             ))}
