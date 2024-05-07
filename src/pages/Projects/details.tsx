92554f8d30fcf31c77f35fab983169616890cff7
sgarnica1 <sgarnica1902@gmail.com>
2024-05-06 20:39:54 -0600

    fix(lint): fix lint erros

--- src/pages/Projects/details.tsx
+++ src/pages/Projects/details.tsx
@@ -177,7 +177,7 @@ const ProjectDetails = () => {
       <section className='flex justify-between my-6'>
         <h1 className='text-[30px] text-gold'>Project Tasks</h1>
         <Link to={id ? APIPath.CREATE_TASK.replace(':projectId', id) : ''}>
-          <AddButton onClick={() => { }} />
+          <AddButton onClick={() => {}} />
         </Link>
       </section>
       <Card className='bg-white' sx={{ Maxwidth: '300px', padding: '20px' }}>
