200a23cfb627b7834be09569c5dfbd2add7a767a
sgarnica1 <sgarnica1902@gmail.com>
2024-05-08 11:57:22 -0600

    fix(company): add back button and remove delete button

--- src/pages/Clients/ClientDetails/ClientDetails.tsx
+++ src/pages/Clients/ClientDetails/ClientDetails.tsx
@@ -2,7 +2,7 @@ import { Chip } from '@mui/material';
 import Divider from '@mui/material/Divider';
 import { useEffect, useState } from 'react';
 import ArchiveModal from '../../../components/common/ArchiveModal';
-import DeleteModal from '../../../components/common/DeleteModal';
+// import DeleteModal from '../../../components/common/DeleteModal';
 import useHttp from '../../../hooks/useHttp';
 import { CompanyEntity } from '../../../types/company';
 import { Response } from '../../../types/response';
@@ -12,11 +12,13 @@ import { ProjectsClientList } from '../../Projects/ProjectsClientList';
 import AbcOutlinedIcon from '@mui/icons-material/AbcOutlined';
 import ArchiveOutlinedIcon from '@mui/icons-material/ArchiveOutlined';
 import BusinessOutlinedIcon from '@mui/icons-material/BusinessOutlined';
-import DeleteOutlineOutlinedIcon from '@mui/icons-material/DeleteOutlineOutlined';
+// import DeleteOutlineOutlinedIcon from '@mui/icons-material/DeleteOutlineOutlined';
 import EditOutlinedIcon from '@mui/icons-material/EditOutlined';
 import EmailOutlinedIcon from '@mui/icons-material/EmailOutlined';
 import StayPrimaryPortraitOutlinedIcon from '@mui/icons-material/StayPrimaryPortraitOutlined';
+import { Box } from '@mui/joy';
 import { useParams } from 'react-router-dom';
+import GoBack from '../../../components/common/GoBack';
 import EditClientFormModal from '../../../components/modules/Clients/EditClientFormModal';
 import { formatDate } from '../../../utils/methods';
 
@@ -31,7 +33,7 @@ import { formatDate } from '../../../utils/methods';
  */
 
 const ClientDetails = () => {
-  const [openDelete, setOpenDelete] = useState<boolean>(false);
+  // const [openDelete, setOpenDelete] = useState<boolean>(false);
   const [openArchive, setOpenArchive] = useState<boolean>(false);
   const [editModalOpen, setEditModalOpen] = useState(false);
   const [company, setCompany] = useState<CompanyEntity | null>(null);
@@ -86,97 +88,98 @@ const ClientDetails = () => {
     return <div>Error: {error.message}</div>;
   }
 
-  const ToggleModalDelete = () => {
-    setOpenDelete(!openDelete);
-  };
-  const ToggleModalArchive = () => {
-    setOpenArchive(!openArchive);
-  };
+  const ToggleModalArchive = () => setOpenArchive(!openArchive);
+  // const ToggleModalDelete = () => setOpenDelete(!openDelete)
 
   return (
-    <main className='bg-white rounded-xl p-6'>
-      <DeleteModal
-        toggleModal={ToggleModalDelete}
-        open={openDelete}
-        setOpen={setOpenDelete}
-        title={'Delete Client'}
-        description={'Are you sure you want to delete this client?'}
-      ></DeleteModal>
-      <ArchiveModal
-        toggleModal={ToggleModalArchive}
-        open={openArchive}
-        setOpen={setOpenArchive}
-        id={company?.id ?? ''}
-        title={`${company?.archived ? 'Unarchive' : 'Archive'}`}
-        description={`Are you sure you want to ${company?.archived ? 'unarchive' : 'archive'} this client?`}
-        handleArchiveClient={() => {
-          return '';
+    <>
+      <Box
+        sx={{
+          display: 'flex',
+          alignItems: 'center',
+          justifyContent: 'flex-end',
+          marginBottom: '10px',
         }}
-      ></ArchiveModal>
-      {company && !loading && (
-        <section className='flex justify-between'>
-          <h2 className='text-2xl text-gold font-medium'>{company.name}</h2>
-          <section className='flex items-center gap-5'>
-            <Chip
-              size='lg'
-              color='primary'
-              variant='outlined'
-              label={formatDate(company.constitutionDate ?? null)}
-            />
-            <EditOutlinedIcon
-              sx={{ width: '30px', height: '30px', cursor: 'pointer' }}
-              className='text-gold'
-              onClick={handleEditClick}
-            />
-            {company && (
-              <EditClientFormModal
-                open={editModalOpen}
-                setOpen={setEditModalOpen}
-                clientData={company}
-                setRefetch={setRefetch}
+      >
+        <GoBack />
+      </Box>
+
+      <main className='bg-white rounded-xl p-6'>
+        <ArchiveModal
+          toggleModal={ToggleModalArchive}
+          open={openArchive}
+          setOpen={setOpenArchive}
+          id={company?.id ?? ''}
+          title={`${company?.archived ? 'Unarchive' : 'Archive'}`}
+          description={`Are you sure you want to ${company?.archived ? 'unarchive' : 'archive'} this client?`}
+          handleArchiveClient={() => {
+            return '';
+          }}
+        ></ArchiveModal>
+        {company && !loading && (
+          <section className='flex justify-between'>
+            <h2 className='text-2xl text-gold font-medium'>{company.name}</h2>
+            <section className='flex items-center gap-5'>
+              <Chip
+                color='primary'
+                variant='outlined'
+                label={formatDate(company.constitutionDate ?? null)}
               />
-            )}
-            <ArchiveOutlinedIcon
-              sx={{ width: '30px', height: '30px', cursor: 'pointer' }}
-              className='text-gold'
-              onClick={ToggleModalArchive}
-            />
-            <DeleteOutlineOutlinedIcon
+              <EditOutlinedIcon
+                sx={{ width: '30px', height: '30px', cursor: 'pointer' }}
+                className='text-gold'
+                onClick={handleEditClick}
+              />
+              {company && (
+                <EditClientFormModal
+                  open={editModalOpen}
+                  setOpen={setEditModalOpen}
+                  clientData={company}
+                  setRefetch={setRefetch}
+                />
+              )}
+              <ArchiveOutlinedIcon
+                sx={{ width: '30px', height: '30px', cursor: 'pointer' }}
+                className='text-gold'
+                onClick={ToggleModalArchive}
+              />
+              {/* <DeleteOutlineOutlinedIcon
               sx={{ width: '30px', height: '30px', cursor: 'pointer' }}
               className='text-gold'
               onClick={() => {
                 ToggleModalDelete();
               }}
-            />
+            /> */}
+            </section>
           </section>
-        </section>
-      )}
+        )}
 
-      {!loading && company && (
-        <>
-          <section className='flex mt-8 font-montserrat justify-start gap-28'>
-            <article className='flex gap-4'>
-              <EmailOutlinedIcon />
-              <p>{company.email}</p>
-            </article>
-            <article className='flex gap-4'>
-              <AbcOutlinedIcon />
-              <p>{company.rfc}</p>
-            </article>
-            <article className='flex gap-4'>
-              <BusinessOutlinedIcon />
-              <p>{company.taxResidence}</p>
-            </article>
-            <article className='flex gap-4'>
-              <StayPrimaryPortraitOutlinedIcon />
-              <p>{company.phoneNumber}</p>
-            </article>
-          </section>
-        </>
-      )}
-      <Divider sx={{ 'margin-top': '30px' }} />
-      <ProjectsClientList clientId={clientId ?? ''} />
-    </main>
+        {!loading && company && (
+          <>
+            <section className='flex mt-8 font-montserrat justify-start gap-28'>
+              <article className='flex gap-4'>
+                <EmailOutlinedIcon />
+                <p>{company.email}</p>
+              </article>
+              <article className='flex gap-4'>
+                <AbcOutlinedIcon />
+                <p>{company.rfc}</p>
+              </article>
+              <article className='flex gap-4'>
+                <BusinessOutlinedIcon />
+                <p>{company.taxResidence}</p>
+              </article>
+              <article className='flex gap-4'>
+                <StayPrimaryPortraitOutlinedIcon />
+                <p>{company.phoneNumber}</p>
+              </article>
+            </section>
+          </>
+        )}
+        <Divider sx={{ 'margin-top': '30px' }} />
+        <ProjectsClientList clientId={clientId ?? ''} />
+      </main>
+    </>
   );
 };
 
