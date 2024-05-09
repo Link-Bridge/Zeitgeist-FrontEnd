13321959ce45eda80780daafd9ff89c1663e5e5e
RodrigoTeran <rodrigo.teranhernandez@gmail.com>
2024-05-09 14:56:53 -0600

    fix(task): fix dates font-size

--- src/pages/Tasks/task-detail.tsx
+++ src/pages/Tasks/task-detail.tsx
@@ -196,7 +196,7 @@ const Task: React.FC = () => {
 
                 {data.endDate && (
                   <Box>
-                    <p style={{ fontSize: '.5rem' }}>Due date</p>
+                    <p style={{ fontSize: '.9rem' }}>Due date</p>
                     <Box
                       sx={{
                         display: 'flex',
