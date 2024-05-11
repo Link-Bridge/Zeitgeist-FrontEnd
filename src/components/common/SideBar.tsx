ff05da70525dd38ae992e14b41ed377e5758f4b9
salgue441 <carlossalguero441@gmail.com>
2024-05-11 17:35:37 -0600

    feat(sidebar): highlights the selected view

--- src/components/common/SideBar.tsx
+++ src/components/common/SideBar.tsx
@@ -1,4 +1,5 @@
 import { FolderShared, Home, SwitchAccount, Toc, ViewTimeline } from '@mui/icons-material';
+import { useState } from 'react';
 import { Link } from 'react-router-dom';
 import LogoZeitgeist from '../../assets/icons/LOGO_Zeitgeist.svg';
 import colors from '../../colors';
@@ -13,6 +14,11 @@ const Items = [
 ];
 
 const SideBar = () => {
+  const [activeItem, setActiveItem] = useState<RoutesPath>(RoutesPath.HOME);
+  const handleItemClick = (href: string) => {
+    setActiveItem(href as RoutesPath);
+  };
+
   return (
     <aside className="relative bg-[url('/src/assets/marmol.jpg')] bg-cover h-screen top-0 left-0 flex flex-col items-center pt-16 gap-10 w-[200px]">
       <div className='absolute top-0 left-0 w-full h-full bg-black bg-opacity-50'></div>
@@ -28,11 +34,15 @@ const SideBar = () => {
               <li
                 key={item.href}
                 className='first:mt-0 my-6 text-base hover:bg-darkestGray transition-all duration-400 font-semibold'
+                onClick={() => handleItemClick(item.href)}
               >
                 <Link
                   to={item.href}
-                  className='flex items-center gap-3 px-9 py-5'
-                  style={{ color: colors.lightGold }}
+                  className='flex items-center gap-3 px-9 py-5 opacity'
+                  style={{
+                    color: colors.lightGold,
+                    opacity: activeItem === item.href ? 1 : 0.7,
+                  }}
                 >
                   <item.icon></item.icon>
                   <p>{item.title}</p>
