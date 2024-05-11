778a4f5a0fe1bf27858c6d5c1ad19b13ce6c2016
Daniel Hurtado <a01707774@tec.mx>
2024-05-11 00:19:11 -0600

    feat(employee): use muy table

--- src/components/modules/Employees/EmployeeeTable.tsx
+++ src/components/modules/Employees/EmployeeeTable.tsx
@@ -1,13 +1,17 @@
 import { DeleteOutline, KeyboardArrowDown } from '@mui/icons-material';
 import { Avatar, Chip, IconButton, Option, Select, Table, selectClasses } from '@mui/joy';
+import { TableBody, TableCell, TableHead, TableRow } from '@mui/material';
 import axios from 'axios';
 import { useContext, useEffect, useState } from 'react';
 import colors from '../../../colors';
 import { EmployeeContext } from '../../../hooks/employeeContext';
 import { SnackbarContext } from '../../../hooks/snackbarContext';
 import useHttp from '../../../hooks/useHttp';
+import { EmployeeOptions } from '../../../pages/Employees/employee-options.eum';
 import { Response } from '../../../types/response';
+import { Role } from '../../../types/role';
 import { BASE_API_URL, RequestMethods } from '../../../utils/constants';
+import ComponentPlaceholder from '../../common/ComponentPlaceholder';
 import DeleteModal from '../../common/DeleteModal';
 import Loader from '../../common/Loader';
 
@@ -30,18 +34,12 @@ type Employee = {
   idRole: string;
 };
 
-interface Role {
-  id: string;
-  title: string;
-  createdAt: Date;
-  updatedAt: Date | null;
-}
-
 interface Props {
   searchTerm: string;
+  selectedOptions: string[];
 }
 
-export default function EmployeeTable({ searchTerm }: Props) {
+export default function EmployeeTable({ searchTerm, selectedOptions }: Props) {
   const { setState } = useContext(SnackbarContext);
   const [open, setOpen] = useState(false);
   const reqEmployees = useHttp<Response<Employee>>(`/employee`, RequestMethods.GET);
@@ -62,19 +60,37 @@ export default function EmployeeTable({ searchTerm }: Props) {
   }, [reqEmployees.error, setState]);
 
   useEffect(() => {
-    if (reqEmployees.data) {
+    if (reqEmployees.data && reqRoles.data) {
       const filteredEmployees = reqEmployees.data?.data.filter(employee => {
-        const fullName = `${employee.firstName} ${employee.lastName}`;
-        return fullName.toLowerCase().includes(searchTerm.toLowerCase());
+        let matchesFilter = false;
+        switch (selectedOptions[0]) {
+          case EmployeeOptions.Name:
+            const fullName = `${employee.firstName} ${employee.lastName}`
+              .toLowerCase()
+              .includes(searchTerm.toLowerCase());
+            matchesFilter = fullName;
+            break;
+          case EmployeeOptions.Email:
+            matchesFilter = employee.email.toLowerCase().includes(searchTerm.toLowerCase());
+            break;
+          case EmployeeOptions.Role:
+            const role = reqRoles.data!.data.find(role => role.id === employee.idRole);
+            const roleTitle = role ? role.title : '';
+            matchesFilter = roleTitle.includes(searchTerm.toLowerCase());
+            break;
+          default:
+            matchesFilter = true;
+        }
+        return matchesFilter;
       });
-      setSearchResults(filteredEmployees as Employee[]);
+      setSearchResults(filteredEmployees);
     }
-  }, [reqEmployees.data, searchTerm]);
+  }, [reqEmployees.data, searchTerm, selectedOptions, reqRoles.data]);
 
-  const handleRolChange = (newRoleId: string, userId: string): void => {
-    if (newRoleId === undefined || userId === undefined) return;
-    const doFetch = async (): Promise<void> => {
-      await axios.put(
+  const handleRolChange = async (newRoleId: string, userId: string) => {
+    if (!newRoleId || !userId) return;
+    try {
+      const response = await axios.put(
         `${BASE_API_URL}/admin/role`,
         {
           userId: userId,
@@ -82,9 +98,23 @@ export default function EmployeeTable({ searchTerm }: Props) {
         },
         { headers: { Authorization: `Bearer ${localStorage.getItem('idToken')}` } }
       );
-      reqEmployees.sendRequest();
-    };
-    void doFetch();
+
+      if (response.status === 200) {
+        const updatedEmployees = searchResults.map(employee => {
+          if (employee.id === userId) {
+            return { ...employee, idRole: newRoleId };
+          }
+          return employee;
+        });
+        setSearchResults(updatedEmployees);
+        setState({ open: true, message: 'Role updated successfully', type: 'success' });
+      } else {
+        console.error('Error updating role:', response.status);
+      }
+    } catch (error) {
+      console.error('Error in handleRolChange:', error);
+      setState({ open: true, message: 'Failed to update role', type: 'danger' });
+    }
   };
 
   const handleDeleteEmployee = (id: string) => {
@@ -96,83 +126,83 @@ export default function EmployeeTable({ searchTerm }: Props) {
   const { employee: employeeContext } = useContext(EmployeeContext);
   const sessionEmployee = employeeContext?.employee.id as string;
 
+  if (reqEmployees.loading || reqRoles.loading) return <Loader />;
+
+  if (reqEmployees.data?.data.length === 0)
+    return <ComponentPlaceholder text='No employees found' />;
+
   return (
     <>
-      {reqEmployees.loading ? (
-        <Loader />
-      ) : (
-        <Table variant={'outlined'}>
-          <>
-            <thead>
-              <tr>
-                <th style={{ width: '10%' }}> Photo </th>
-                <th>Name</th>
-                <th>Role</th>
-                <th>Email</th>
-                <th style={{ width: '10%' }}></th>
-              </tr>
-            </thead>
-            <tbody>
-              {searchResults.map(employee => (
-                <tr key={employee.id}>
-                  <td>
-                    {employee.imageUrl ? <Avatar src={employee.imageUrl}></Avatar> : <Avatar />}
-                  </td>
-                  <td>
-                    {employee.firstName} {employee.lastName}{' '}
-                  </td>
-                  <td>
-                    <Select
-                      variant='outlined'
-                      color='neutral'
-                      indicator={<KeyboardArrowDown />}
-                      defaultValue={employee.idRole}
-                      disabled={sessionEmployee === employee.id}
-                      onChange={e => {
-                        if (e === null) return;
-                        handleRolChange((e.target as unknown)?.ariaLabel || '', employee.id);
+      <Table>
+        <TableHead>
+          <TableRow>
+            <TableCell sx={{ width: '10%' }}>Photo</TableCell>
+            <TableCell>Name</TableCell>
+            <TableCell>Role</TableCell>
+            <TableCell>Email</TableCell>
+            <TableCell sx={{ width: '15%', textAlign: 'right' }}></TableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {searchResults.map(employee => (
+            <TableRow key={employee.id}>
+              <TableCell>
+                {employee.imageUrl ? <Avatar src={employee.imageUrl} /> : <Avatar />}
+              </TableCell>
+              <TableCell>
+                {employee.firstName} {employee.lastName}
+              </TableCell>
+              <TableCell>
+                <Select
+                  variant='outlined'
+                  color='neutral'
+                  indicator={<KeyboardArrowDown />}
+                  defaultValue={employee.idRole}
+                  disabled={sessionEmployee === employee.id}
+                  onChange={e => {
+                    if (!e || e === null) return;
+                    handleRolChange((e.target as unknown)?.ariaLabel || '', employee.id);
+                  }}
+                  sx={{
+                    width: 150,
+                    [`& .${selectClasses.indicator}`]: {
+                      transition: '0.2s',
+                      [`&.${selectClasses.expanded}`]: {
+                        transform: 'rotate(-180deg)',
+                      },
+                    },
+                  }}
+                >
+                  {reqRoles.data?.data.map((role: Role) => (
+                    <Option aria-label={role.id} key={role.id} value={role.id}>
+                      {toTitleCase(role.title)}
+                    </Option>
+                  ))}
+                </Select>
+              </TableCell>
+              <TableCell>
+                <Chip className='w-full overflow' variant='soft'>
+                  {employee.email}
+                </Chip>
+              </TableCell>
+              <TableCell sx={{ textAlign: 'right' }}>
+                {' '}
+                {sessionEmployee !== employee.id && (
+                  <IconButton>
+                    <DeleteOutline
+                      onClick={() => {
+                        setOpen(true);
+                        setCurrentEmployeeId(employee.id);
                       }}
-                      sx={{
-                        width: 250,
-                        [`& .${selectClasses.indicator}`]: {
-                          transition: '0.2s',
-                          [`&.${selectClasses.expanded}`]: {
-                            transform: 'rotate(-180deg)',
-                          },
-                        },
-                      }}
-                    >
-                      {reqRoles.data?.data.map((role: Role) => (
-                        <Option aria-label={role.id} key={role.id} value={role.id}>
-                          {toTitleCase(role.title)}
-                        </Option>
-                      ))}
-                    </Select>
-                  </td>
-                  <td>
-                    <Chip className='w-full overflow' variant='soft'>
-                      {employee.email}
-                    </Chip>
-                  </td>
-                  <td>
-                    {sessionEmployee !== employee.id && (
-                      <IconButton>
-                        <DeleteOutline
-                          onClick={() => {
-                            setOpen(true);
-                            setCurrentEmployeeId(employee.id);
-                          }}
-                          style={{ color: colors.gold }}
-                        />
-                      </IconButton>
-                    )}
-                  </td>
-                </tr>
-              ))}
-            </tbody>
-          </>
-        </Table>
-      )}
+                      style={{ color: colors.gold }}
+                    />
+                  </IconButton>
+                )}
+              </TableCell>
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
       <DeleteModal
         open={open}
         title='Delete Employee'
---
+++ src/types/role.ts
@@ -0,0 +1,6 @@
+export interface Role {
+  id: string;
+  title: string;
+  createdAt: Date;
+  updatedAt: Date | null;
+}
