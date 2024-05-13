a4c024468534fa565fd256ab9451875e69998a1d
Ian326 <a01708940@tec.mx>
2024-05-13 11:26:41 -0600

    fix(RF27): Loader not displaying properly

--- src/pages/Clients/main.tsx
+++ src/pages/Clients/main.tsx
@@ -84,6 +84,51 @@ const ClientList = (): JSX.Element => {
     setOpen(true);
   };
 
+  if (isLoading) {
+    return (
+      <main className='flex flex-col gap-2 flex-1 min-h-0'>
+        <section className='flex justify-between items-center w-full p-2'>
+          <SearchBar
+            searchTerm={searchTerm}
+            setSearchTerm={setSearchTerm}
+            placeholder='Search by name'
+            setSelectedOption={() => {}}
+            options={[]}
+          />
+          <div className='flex flex-row items-center gap-2'>
+            {isAdmin && (
+              <div className='flex flex-row items-center gap-2'>
+                <div className='flex items-center gap-2'>
+                  <FilterAltIcon
+                    sx={{ width: '30px', height: '30px', cursor: 'pointer' }}
+                    className='text-gold'
+                  />
+                  <p>Filter Clients:</p>
+                </div>
+                <GenericDropdown
+                  defaultValue={CompanyFilters.ALL}
+                  options={[
+                    CompanyFilters.ALL,
+                    CompanyFilters.NOT_ARCHIVED,
+                    CompanyFilters.ARCHIVED,
+                  ]}
+                  onChange={value => handleFilter(value)}
+                />
+              </div>
+            )}
+            <AddButton onClick={openModal} />
+          </div>
+        </section>
+        <NewClientFormModal open={open} setOpen={setOpen} setRefetch={setRefetch} />
+        <section className='flex-1 overflow-scroll'>
+          <div className='bg-cardBg rounded-xl flex-1 grid md:grid-cols-2 lg:grid-cols-3 min-h-0 shadow-lg p-4 gap-5'>
+            <Loader />
+          </div>
+        </section>
+      </main>
+    );
+  }
+
   return (
     <main className='flex flex-col gap-2 flex-1 min-h-0'>
       <section className='flex justify-between items-center w-full p-2'>
@@ -120,21 +165,17 @@ const ClientList = (): JSX.Element => {
       ) : (
         <section className='flex-1 overflow-scroll'>
           <div className='bg-cardBg rounded-xl flex-1 grid md:grid-cols-2 lg:grid-cols-3 min-h-0 shadow-lg p-4 gap-5'>
-            {isLoading ? (
-              <Loader />
-            ) : (
-              filteredCompanies.map(company => (
-                <Link to={`${RoutesPath.CLIENTS}/details/${company.id}`} key={company.id}>
-                  <ClientCard
-                    name={truncateText(company.name)}
-                    accountingHours={company.accountingHours || 0}
-                    legalHours={company.legalHours || 0}
-                    chargeableHours={company.chargeableHours || 0}
-                    totalProjects={company.totalProjects || 0}
-                  />
-                </Link>
-              ))
-            )}
+            {filteredCompanies.map(company => (
+              <Link to={`${RoutesPath.CLIENTS}/details/${company.id}`} key={company.id}>
+                <ClientCard
+                  name={truncateText(company.name)}
+                  accountingHours={company.accountingHours || 0}
+                  legalHours={company.legalHours || 0}
+                  chargeableHours={company.chargeableHours || 0}
+                  totalProjects={company.totalProjects || 0}
+                />
+              </Link>
+            ))}
           </div>
         </section>
       )}
