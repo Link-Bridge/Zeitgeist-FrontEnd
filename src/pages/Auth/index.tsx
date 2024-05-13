9efa7f021a62d4a10be09ac04af265c95ac4fb62
Daniel Hurtado <a01707774@tec.mx>
2024-05-12 19:42:20 -0600

    feat(token): refresh

--- src/pages/Auth/index.tsx
+++ src/pages/Auth/index.tsx
@@ -1,6 +1,6 @@
 import Button from '@mui/joy/Button';
 import { signInWithPopup } from 'firebase/auth';
-import React, { useContext, useState } from 'react';
+import React, { useContext, useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
 import googleImage from '../../assets/images/google-logo.webp';
 import loginImage from '../../assets/images/login-image.png';
@@ -13,83 +13,67 @@ import { RequestMethods, RoutesPath } from '../../utils/constants';
 import { handleGetDeviceToken } from './device-token';
 
 const Auth: React.FC = () => {
-  const [user, setUser] = useState<EmployeeReponse>();
-  const [idToken, setIdToken] = useState<string>('');
   const navigate = useNavigate();
   const { setEmployee } = useContext(EmployeeContext);
   const { setState } = useContext(SnackbarContext);
-  const { sendRequest, data } = useHttp<EmployeeReponse>('/employee/signup', RequestMethods.POST);
+  const { sendRequest, data, loading } = useHttp<EmployeeReponse>(
+    '/employee/signup',
+    RequestMethods.POST
+  );
 
-  const createUser = async () => {
-    sendRequest({}, {});
-    setEmployee(data!);
-    setUser(data!);
-    if (user?.role !== 'No role') {
-      navigate(RoutesPath.HOME);
-    } else {
-      setState({ open: true, message: 'User not authorized', type: 'danger' });
+  const refreshToken = async () => {
+    try {
+      const currentUser = auth.currentUser;
+      if (currentUser) {
+        const token = await currentUser.getIdToken(true);
+        localStorage.setItem('idToken', token);
+        console.log('Token refreshed!');
+      }
+    } catch (error) {
+      console.error('Error refreshing token:', error);
     }
   };
 
   const handleGoogleSignIn = async () => {
     try {
       const result = await signInWithPopup(auth, provider);
-      const token = await result.user.getIdToken();
-      setIdToken(token);
-      localStorage.setItem('idToken', idToken);
-      createUser();
-      handleGetDeviceToken(user!.employee.email);
+      const token = await result.user.getIdToken(true);
+      const refreshToken = result.user.refreshToken;
+      localStorage.setItem('idToken', token);
+      localStorage.setItem('refreshToken', refreshToken);
+
+      await sendRequest({}, {});
+      console.log('data ', data);
+      if (!loading) {
+        updateUserContext();
+      }
     } catch (error) {
-      console.error('Error signing in with google:', error);
-      setState({ open: true, message: 'Failed to sign in with google', type: 'danger' });
-      throw error;
+      setState({ open: true, message: 'Oops! we are having some troubles', type: 'danger' });
     }
   };
 
-  // const handleGoogleSignIn = async () => {
-  //   try {
+  const updateUserContext = async () => {
+    if (data) {
+      if (data.data.role !== 'No role') {
+        console.log('data ', data);
+        setEmployee(data.data);
+        localStorage.setItem('employee', JSON.stringify(data));
+        navigate(RoutesPath.HOME);
+        handleGetDeviceToken(data.data.employee.email);
+      } else {
+        setState({
+          open: true,
+          message: 'User not authorized',
+          type: 'danger',
+        });
+      }
+    }
+  };
 
-  //     if (!response.ok) {
-  //       throw new Error('Failed to sign up');
-  //     }
-
-  //     const responseData = await response.json();
-  //     setEmployee(responseData.data);
-  //     localStorage.setItem('employee', JSON.stringify(responseData.data));
-
-  //     if (responseData.data.role === 'No role' || !responseData.data.role) {
-  //       throw new Error('User not authorized');
-  //     }
-
-  //     handleGetDeviceToken(result.user.email);
-
-  //     navigate(RoutesPath.HOME);
-  //   } catch (error) {
-  //     setState({ open: true, message: (error as Error).message, type: 'danger' });
-  //     throw error;
-  //   }
-  // };
-
-  // useEffect(() => {
-  //   try {
-  //     if (data && idToken) {
-  //       setUser(data);
-  //       if (data.employee) {
-  //         setEmployee(data);
-  //       }
-  //       localStorage.setItem('employee', JSON.stringify(data));
-  //       if (data.role === 'No role' || !data.role) {
-  //         setState({ open: true, message: 'User not authorized', type: 'danger' });
-  //       }
-  //       handleGetDeviceToken(data.employee.email);
-  //       navigate(RoutesPath.HOME);
-  //     }
-  //   } catch (error) {
-  //     console.error('Error signing in with google:', error);
-  //     setState({ open: true, message: 'Failed to sign in with google', type: 'danger' });
-  //     throw error;
-  //   }
-  // }, [data, navigate, idToken, setState, setEmployee]);
+  useEffect(() => {
+    const intervalId = setInterval(refreshToken, 18000);
+    return () => clearInterval(intervalId);
+  }, []);
 
   return (
     <div className='bg-cover bg-center h-screen' style={{ backgroundImage: `url(${loginImage})` }}>
