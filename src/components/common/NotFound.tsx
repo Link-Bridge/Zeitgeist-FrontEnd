5e2f073e47b8eea2fe333e016c7da6ca6ef1748b
Daniel Hurtado <a01707774@tec.mx>
2024-05-14 22:45:02 -0600

    fix(lottie): add ts dependecies

--- src/components/common/NotFound.tsx
+++ src/components/common/NotFound.tsx
@@ -1,16 +1,7 @@
-import Lottie from 'react-lottie';
+import Lottie from 'react-lottie-player';
 import NotFoundAnimation from '../../assets/not-found.json';
 
 const NotFoundPage = () => {
-  const defaultOptions = {
-    loop: true,
-    autoplay: true,
-    animationData: NotFoundAnimation,
-    rendererSettings: {
-      preserveAspectRatio: 'xMidYMid slice',
-    },
-  };
-
   return (
     <div
       style={{
@@ -24,7 +15,7 @@ const NotFoundPage = () => {
         left: 0,
       }}
     >
-      <Lottie options={defaultOptions} height={800} width={800} />
+      <Lottie animationData={NotFoundAnimation} play loop style={{ width: 800, height: 800 }} />
     </div>
   );
 };
