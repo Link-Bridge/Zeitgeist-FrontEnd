1156eb51d03e945fb0c9f08bc6d630b30f8e8c2f
Ian326 <a01708940@tec.mx>
2024-05-15 12:53:04 -0600

    fix(RF19RF19): Responsiveness for Create/Modify Clients pages

--- src/components/modules/Clients/NewClientFormModal.tsx
+++ src/components/modules/Clients/NewClientFormModal.tsx
@@ -18,16 +18,17 @@ import CreateClientButton from './CreateClientButton';
  * @brief style object for box component
  */
 const style = {
-  position: 'absolute' as const,
+  position: 'fixed',
   top: '50%',
   left: '50%',
   transform: 'translate(-50%, -50%)',
-  width: 620,
   bgcolor: 'background.paper',
   border: '2px solid' && colors.darkGold,
   boxShadow: 24,
   p: 4,
   borderRadius: 3,
+  minWidth: '448px',
+  maxWidth: '50vw',
 };
 
 interface NewClientFormModalProps {
