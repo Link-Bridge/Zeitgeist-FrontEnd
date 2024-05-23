d08b0c332ded63ebcfbd26e7608b487cad9b3eb5
Ricardo Fernandez <ricardo.a.fernandez.a@gmail.com>
2024-05-23 17:14:39 -0600

    fix(reqm): new link to changes folder

--- src/components/common/Header.tsx
+++ src/components/common/Header.tsx
@@ -39,7 +39,7 @@ const Header = ({ pageTitle }: HeaderProps) => {
         </a>
         {employeeContext?.employee.imageUrl ? (
           <Avatar
-            src={employeeContext.employee.imageUrl ?? ''}
+            src={employeeContext.employee.imageUrl}
             alt={employeeContext.employee.firstName}
           />
         ) : (
