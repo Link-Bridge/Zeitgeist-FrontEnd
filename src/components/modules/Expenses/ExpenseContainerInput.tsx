137f171fc158ed138a2ac146849594b5dec817b8
sgarnica1 <sgarnica1902@gmail.com>
2024-06-04 11:11:01 -0600

    fix(ui): update ui

--- src/components/modules/Expenses/ExpenseContainerInput.tsx
+++ src/components/modules/Expenses/ExpenseContainerInput.tsx
@@ -83,7 +83,7 @@ const ExpenseContainerInput = ({
 
   return (
     <>
-      <div className={`flex flex-wrap items-start gap-3 ${index === 0 ? 'mt-0' : 'mt-10'}`}>
+      <div className={`flex flex-wrap items-start gap-3 ${index === 0 ? 'mt-0' : 'mt-10'} justify-end`}>
         <p className='text-[#686868] font-semibold text-base'>{`${index + 1}.`}</p>
         <main className='flex flex-col flex-1'>
           <section className='flex flex-col md:flex-row flex-1 gap-4'>
