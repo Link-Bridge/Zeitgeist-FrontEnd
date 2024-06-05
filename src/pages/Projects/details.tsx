ee2ba164bf66a3c0936ff9bf8c8b7f24aa3e90f3
sgarnica1 <sgarnica1902@gmail.com>
2024-06-05 10:00:33 -0600

    fix(projects): fix responsiveness

--- src/pages/Projects/details.tsx
+++ src/pages/Projects/details.tsx
@@ -284,8 +284,8 @@ const ProjectDetails = () => {
         sx={{ Maxwidth: '300px', padding: '20px', border: 'none' }}
       >
         <section className='font-montserrat'>
-          <section className='flex flex-wrap flex-col-reverse justify-between gap-y-2'>
-            <h3 className='text-4xl font-medium whitespace-break-spaces break-all'>{data?.name}</h3>
+          <section className='flex flex-wrap flex-col-reverse lg:flex-row justify-between gap-y-2'>
+            <h3 className='text-2xl lg:text-3xl font-medium whitespace-break-spaces break-all'>{data?.name}</h3>
             <div className='flex flex-wrap gap-3 mb-6 justify-end'>
               <Button
                 component={Link}
@@ -439,7 +439,7 @@ const ProjectDetails = () => {
           Project Tasks
         </h1>
         <Link to={id ? `${RoutesPath.TASKS}/${id}/create` : RoutesPath.TASKS}>
-          <AddButton onClick={() => {}} />
+          <AddButton onClick={() => { }} />
         </Link>
       </section>
       <Card className='bg-white overflow-auto mb-4'>
