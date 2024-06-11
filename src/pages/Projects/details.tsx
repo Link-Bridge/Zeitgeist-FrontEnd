6eb865256957e3e7d200ce732ac1bd44ebe3284f
sgarnica1 <sgarnica1902@gmail.com>
2024-06-10 19:12:59 -0600

    fix(lint): fix lint errors

--- src/pages/Projects/details.tsx
+++ src/pages/Projects/details.tsx
@@ -539,7 +539,7 @@ const ProjectDetails = () => {
             </Button>
           )}
           <Link to={id ? `${RoutesPath.TASKS}/${id}/create` : RoutesPath.TASKS}>
-            <AddButton onClick={() => { }} />
+            <AddButton onClick={() => {}} />
           </Link>
         </Box>
       </section>
